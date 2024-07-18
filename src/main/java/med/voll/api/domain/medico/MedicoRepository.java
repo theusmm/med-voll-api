370484f10a0d0700a4002663dc328956288669f5
theusmm <theusmm75@gmail.com>
2024-07-18 19:42:13 -0300

    Automation testing

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -11,22 +11,19 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 
     @Query("""
-            select m from Medico m
-            where
-            m.ativo = true
-            and
-            m.especialidade = :especialidade
-            and
-            m.id not in(
-                select c.medico.id from Consulta c
-                where
-                c.data = :data
-        and
-                c.motivoCancelamento is null
-            )
-            order by rand()
-            limit 1
-""")
+            SELECT m 
+            FROM Medico m
+            WHERE m.ativo = true
+            AND m.especialidade = :especialidade
+            AND m.id NOT IN (
+                SELECT c.medico.id
+                FROM Consulta c
+                WHERE c.data = :data
+                AND c.motivoCancelamento is null
+                )
+            ORDER BY RANDOM()
+            LIMIT 1
+    """)
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
 
     @Query("""
